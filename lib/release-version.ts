633df438c455a96e2fa9787d0e9a78b3246c902f
kandykbayevtagir-wq <kandykbayevtagir@gmail.com>
2026-08-07 09:01:46 +0500

    v0.16.9: bump release identity

--- lib/release-version.ts
+++ lib/release-version.ts
@@ -1,4 +1,4 @@
-export const CURRENT_RELEASE_VERSION = "v0.16.8";
-export const CURRENT_RELEASE_BADGE = "v0.16.8";
-export const CURRENT_RELEASE_CODENAME = "Resilience Core";
+export const CURRENT_RELEASE_VERSION = "v0.16.9";
+export const CURRENT_RELEASE_BADGE = "v0.16.9";
+export const CURRENT_RELEASE_CODENAME = "Interface Polish";
 export const CURRENT_RELEASE_CHANNEL = "preview" as const;
