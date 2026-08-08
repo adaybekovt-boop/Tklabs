93eaa75582d037af787815fdc25b8c3e26d4c908
kandykbayevtagir-wq <kandykbayevtagir@gmail.com>
2026-08-09 03:28:42 +0500

    chore: bump release identity to v0.24.0

--- lib/release-version.ts
+++ lib/release-version.ts
@@ -1,4 +1,4 @@
-export const CURRENT_RELEASE_VERSION = "v0.23.4";
-export const CURRENT_RELEASE_BADGE = "v0.23.4";
-export const CURRENT_RELEASE_CODENAME = "Erma Alive & Vision";
+export const CURRENT_RELEASE_VERSION = "v0.24.0";
+export const CURRENT_RELEASE_BADGE = "v0.24.0";
+export const CURRENT_RELEASE_CODENAME = "Erma Grounded Search & Kazakhstan Intelligence";
 export const CURRENT_RELEASE_CHANNEL = "preview" as const;
