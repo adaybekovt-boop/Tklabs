f5bb1da12e615dede5d7f190103aa9eb77ffdc4b
kandykbayevtagir-wq <kandykbayevtagir@gmail.com>
2026-08-08 23:25:09 +0500

    v0.23: bump current release identity

--- lib/release-version.ts
+++ lib/release-version.ts
@@ -1,4 +1,4 @@
-export const CURRENT_RELEASE_VERSION = "v0.21.15";
-export const CURRENT_RELEASE_BADGE = "v0.21.15";
-export const CURRENT_RELEASE_CODENAME = "Erma Intelligence Engine";
+export const CURRENT_RELEASE_VERSION = "v0.23.4";
+export const CURRENT_RELEASE_BADGE = "v0.23.4";
+export const CURRENT_RELEASE_CODENAME = "Erma Alive & Vision";
 export const CURRENT_RELEASE_CHANNEL = "preview" as const;
